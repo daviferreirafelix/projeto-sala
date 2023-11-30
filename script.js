a126c5626ed3d9f6c3d627f9d021f44fa29cda59
Davi <adavi9920@gmail.com>
2023-11-29 21:38:46 -0300

    Projeto de Sala

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-const postContainer = document.getElementById('posts-container');
-const loader = document.querySelector('.loader');
-const buscador = document.getElementById('buscar');
+let val = document.getElementById('menu-resp');
+let classe = document.getElementsByClassName('responsivo');
+let classes = document.getElementsByClassName('content');
+let inp = document.getElementById('buscar').innerText;
+
+console.log(classes)
+
+function menuResponsivo(){
+     if (classe.length == 0){
+        val.classList.add('responsivo');
+     } else if (classe.length > 0) {
+        val.classList.remove('responsivo');
+     }
+}
+
+function buscaPost(){
+    for (let i = 0; i > classes.length; i++){
+        let texto = classes[i].innerText;
+
+        if (texto === inp.innerText){
+            console.log("certo")
+        }
+    }
+    
+}
